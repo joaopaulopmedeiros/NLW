5e24756b80221bf6f018c0f65369c96ba08835c6
joao paulo <joaopaulopmedeiros@gmail.com>
2020-06-02 23:42:35 -0300

    adding Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import routes from './routes'
 import path from 'path'
+import cors from 'cors'
 
 const app = express()
 const files = path.resolve(__dirname, '..', 'uploads')
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(files))
